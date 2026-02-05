04aef0ba2100b4499b902103a08af93b9339e875
Hariniriya2703 <harini03mathialagan@gmail.com>
2026-02-05 23:15:10 +0530

    Add files via upload
    Primitive datatypes assignment

---
+++ Datatypes.js
@@ -0,0 +1,11 @@
+let username="Ramesh"
+let organisation='TestLeaf'
+let contactNumber=1234567890
+let knowsAutomation=false
+let usesPlaywright
+
+console.log("The username is "+username +" & the datatype is " +typeof(username))
+console.log("The Organisation name is "+organisation +" & the datatype is " +typeof(organisation))
+console.log("The contact Number is "+contactNumber +" & the datatype is " +typeof(contactNumber))
+console.log("Having knowledge in Automation "+knowsAutomation +" & the datatype is " +typeof(knowsAutomation))
+console.log("Using playwright "+usesPlaywright +" & the datatype is " +typeof(usesPlaywright))
