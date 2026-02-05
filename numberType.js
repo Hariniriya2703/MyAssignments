eb3985455b8540b95f9985ce9c7370126b08400f
Hariniriya2703 <harini03mathialagan@gmail.com>
2026-02-05 23:47:55 +0530

    Add files via upload
    Odd or even & Number type program

---
+++ numberType.js
@@ -0,0 +1,14 @@
+let number=5
+console.log("To Print whether the given number is POSITIVE, NEGATIVE or NEUTRAL number")
+if(number>0)
+{
+    console.log("The given number "+number+ " is a POSITIVE number")
+}
+else if(number<0)
+{
+    console.log("The given number "+number+ " is a NEGATIVE number")
+}
+else
+{
+    console.log("The given number "+number+ " is a NEUTRAL number")
+}
---
+++ oddOREven.js
@@ -0,0 +1,8 @@
+let num=10
+console.log("To print whether the given number is ODD or EVEN number")
+if(num%2!=0)
+{
+    console.log("The given number " +num+ " is ODD number")
+}
+else
+console.log("The given number " +num+ " is EVEN number")
